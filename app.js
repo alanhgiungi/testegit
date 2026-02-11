3dc22efa08498f53de9f2f873cca2612e191fb0a
ALAN HENRIQUE GIUNGI <alan.hgiungi@senacsp.edu.br>
2026-02-10 22:05:41 -0300

    incluir as alterções dos aqruivos app.js teste.html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("olá mundo")
 const variavel = document.getElementById("root")
-variavel.innerHTML = "Meu primeiro código em Javascript. Já sou um programador"
+variavel.innerHTML = "Meu primeiro código em Javascript. Já sou um programador sênior"
+
